6a324bc9c997a42cdd0a4736388c589a7e259f13
Natanael Patrício <nata12nael@gmail.com>
2022-06-02 20:45:15 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija com cuidado`)
